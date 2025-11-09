f0b21b4c33284afe6c24ec6cb34f730d8179ce22
Soham Sagdeo <sagdeosoham@gmail.com>
2025-11-09 12:56:18 +0530

    Update Pract_8.c

--- Pract_8.c
+++ Pract_8.c
@@ -1,87 +1,74 @@
 #include <stdio.h>
-#include <limits.h>
+#include <stdlib.h>
+struct Edge {
+    int src, dest, weight;
+};
+struct Subset {
+    int parent;
+    int rank;
+};
+int find(struct Subset subsets[], int i) {
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    return subsets[i].parent;
+}
+void Union(struct Subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
 
-#define MAX 20
-#define INF 9999
-
-void dijkstra(int n, int graph[MAX][MAX], int start) {
-    int cost[MAX][MAX], distance[MAX], visited[MAX], pred[MAX];
-    int count, mindistance, nextnode, i, j;
-
-    // Create cost matrix
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            if (graph[i][j] == 0)
-                cost[i][j] = INF;
-            else
-                cost[i][j] = graph[i][j];
-
-    // Initialize distances and visited array
-    for (i = 0; i < n; i++) {
-        distance[i] = cost[start][i];
-        pred[i] = start;
-        visited[i] = 0;
-    }
-
-    distance[start] = 0;
-    visited[start] = 1;
-    count = 1;
-
-    while (count < n - 1) {
-        mindistance = INF;
-
-        // Next node = node with minimum distance
-        for (i = 0; i < n; i++)
-            if (distance[i] < mindistance && !visited[i]) {
-                mindistance = distance[i];
-                nextnode = i;
-            }
-
-        visited[nextnode] = 1;
-
-        // Update distance of neighboring nodes
-        for (i = 0; i < n; i++)
-            if (!visited[i])
-                if (mindistance + cost[nextnode][i] < distance[i]) {
-                    distance[i] = mindistance + cost[nextnode][i];
-                    pred[i] = nextnode;
-                }
-        count++;
-    }
-
-    // Print shortest paths and distances
-    printf("\nShortest distances from node %d:\n", start + 1);
-    for (i = 0; i < n; i++) {
-        if (distance[i] == INF)
-            printf("Node %d is unreachable (distance = -1)\n", i + 1);
-        else {
-            printf("To node %d = %d\tPath: %d", i + 1, distance[i], i + 1);
-            j = i;
-            do {
-                j = pred[j];
-                printf(" <- %d", j + 1);
-            } while (j != start);
-            printf("\n");
-        }
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
     }
 }
+int compareEdges(const void *a, const void *b) {
+    struct Edge *a1 = (struct Edge *)a;
+    struct Edge *b1 = (struct Edge *)b;
+    return a1->weight - b1->weight;
+}
+void KruskalMST(struct Edge edges[], int V, int E) {
+    struct Edge result[V]; 
+    int e = 0; 
+    int i = 0; 
+    qsort(edges, E, sizeof(struct Edge), compareEdges);
+    struct Subset *subsets = (struct Subset *)malloc(V * sizeof(struct Subset));
+    for (int v = 0; v < V; v++) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+    while (e < V - 1 && i < E) {
+        struct Edge next_edge = edges[i++];
 
+        int x = find(subsets, next_edge.src);
+        int y = find(subsets, next_edge.dest);
+        if (x != y) {
+            result[e++] = next_edge;
+            Union(subsets, x, y);
+        }
+    }
+    printf("\nEdges in the Minimum Spanning Tree (MST):\n");
+    int totalWeight = 0;
+    for (i = 0; i < e; i++) {
+        printf("%d -- %d == %d\n", result[i].src, result[i].dest, result[i].weight);
+        totalWeight += result[i].weight;
+    }
+    printf("Total weight of MST: %d\n", totalWeight);
+    free(subsets);
+}
 int main() {
-    int n, graph[MAX][MAX], i, j, start;
-
-    printf("Enter number of nodes: ");
-    scanf("%d", &n);
-
-    printf("Enter adjacency matrix (0 for no edge):\n");
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            scanf("%d", &graph[i][j]);
-
-    printf("Enter starting node (1 to %d): ", n);
-    scanf("%d", &start);
-    start--; // adjust for 0-based index
-
-    dijkstra(n, graph, start);
-
+    int V, E;
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &V, &E);
+    struct Edge *edges = (struct Edge *)malloc(E * sizeof(struct Edge));
+    printf("Enter each edge (source destination weight):\n");
+    for (int i = 0; i < E; i++) {
+        scanf("%d %d %d", &edges[i].src, &edges[i].dest, &edges[i].weight);
+    }
+    KruskalMST(edges, V, E);
+    free(edges);
     return 0;
 }
